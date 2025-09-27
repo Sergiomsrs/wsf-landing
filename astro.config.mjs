d92043830efc38d8978f2fa106d20c551b1a9d27
Sergio <sergiomsrs@gmail.com>
2025-09-27 18:13:38 +0200

    Elimina la propiedad base de la configuración de Astro para simplificar la ruta del sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sergiomsrs.github.io/wsf-landing/',
+    base: '/wsf-landing/',
     vite: {
         plugins: [tailwindcss()],
     },
