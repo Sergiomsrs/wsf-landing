58ad286a78b69847537bced7617a9696352829bc
Sergio <sergiomsrs@gmail.com>
2025-09-27 17:55:50 +0200

    Agrega configuración de despliegue para GitHub Pages y define la configuración del proyecto Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://sergiomsrs.github.io/wsf-landing/',
+    base: '/wsf-landing',
     vite: {
         plugins: [tailwindcss()],
     },
