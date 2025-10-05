4ee6060aad90d568dfbb98cdb602e2b3531b0ef5
Sergio <sergiomsrs@gmail.com>
2025-10-05 10:42:13 +0200

    Refactor code structure for improved readability and maintainability

--- src/data/featuresData.ts
+++ src/data/featuresData.ts
@@ -32,7 +32,7 @@ export const featuresData: Feature[] = [
         imageAlt: "Control de turnos desactivados",
         theme: "blue",
         badge: "Cumplimiento legislativo",
-        icon: "M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+        icon: "M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728L5.636 5.636m12.728 12.728L18 17m-5.5-5.5l-2-2"
     },
     {
         id: 3,
@@ -42,27 +42,27 @@ export const featuresData: Feature[] = [
         imageAlt: "Control de Filtros",
         theme: "purple",
         badge: "Organización Eficiente",
-        icon: "M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"
+        icon: "M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
     },
     {
         id: 4,
         title: "Gestión de disponibilidad",
         description: "Aquí puedes gestionar la disponibilidad de los empleados, ajustando su calendario según vacaciones, permisos u otros tipos de ausencia. Cuando un empleado está de vacaciones o tiene un permiso, los campos correspondientes se deshabilitan automáticamente y se resaltan en rojo para una identificación visual clara, asegurando una planificación precisa y eficiente.",
-        imageSrc: `${baseUrl}favicon.svg`,
+        imageSrc: `${baseUrl}control-disponibilidad.webp`,
         imageAlt: "Gestión de disponibilidad",
         theme: "orange",
         badge: "Organización Eficiente",
-        icon: "M15 17h5l-5 5v-5zM4.828 7l10.586 10.586c.781.781 2.047.781 2.828 0l1.414-1.414c.781-.781.781-2.047 0-2.828L9.242 3.828c-.781-.781-2.047-.781-2.828 0L4.828 5.414c-.781.781-.781 2.047 0 2.828z"
+        icon: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
     },
     {
         id: 5,
         title: "Pie de zona de trabajo",
         description: "En esta sección, puedes guardar o restablecer fácilmente el área de trabajo según tus necesidades. También se presenta un resumen detallado del intervalo de tiempo seleccionado, ofreciendo una visión clara de las jornadas laborales de los empleados. Este resumen incluye las horas totales trabajadas, la diferencia con las horas contratadas y un desglose visual de las horas trabajadas por día. Además, en la zona de resumen por días, el sistema detecta automáticamente los días festivos y ajusta la base de horas del empleado en consecuencia, asegurando un cálculo preciso y eficiente.",
-        imageSrc: `${baseUrl}favicon.svg`,
+        imageSrc: `${baseUrl}pie-zona-trabajo.webp`,
         imageAlt: "Pie de zona de trabajo",
-        theme: "orange",
+        theme: "green",
         badge: "Organización Eficiente",
-        icon: "M15 17h5l-5 5v-5zM4.828 7l10.586 10.586c.781.781 2.047.781 2.828 0l1.414-1.414c.781-.781.781-2.047 0-2.828L9.242 3.828c-.781-.781-2.047-.781-2.828 0L4.828 5.414c-.781.781-.781 2.047 0 2.828z"
+        icon: "M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V9a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2z"
     },
     {
         id: 6,
@@ -70,9 +70,29 @@ export const featuresData: Feature[] = [
         description: "Explora un listado detallado de los turnos laborales en un formato claro y estructurado. Esta sección identifica automáticamente los turnos partidos y presenta una vista intuitiva y fácil de entender para cada empleado. Además, ofrece un resumen completo del período seleccionado, incluyendo la base de horas programadas, las horas ya trabajadas y las pendientes por gestionar.",
         imageSrc: `${baseUrl}resumen-semanal.webp`,
         imageAlt: "Resumen de turnos por empleado",
-        theme: "orange",
+        theme: "yellow",
         badge: "Organización Eficiente",
-        icon: "M15 17h5l-5 5v-5zM4.828 7l10.586 10.586c.781.781 2.047.781 2.828 0l1.414-1.414c.781-.781.781-2.047 0-2.828L9.242 3.828c-.781-.781-2.047-.781-2.828 0L4.828 5.414c-.781.781-.781 2.047 0 2.828z"
+        icon: "M9 5H7a2 2 0 00-2 2v6a2 2 0 002 2h2m0 0h2a2 2 0 002-2V7a2 2 0 00-2-2H9m0 0V3m0 2h2m-2 8v2m0 0v2m0-2h2m-2 0H7"
+    },
+    {
+        id: 7,
+        title: "Registro de Jornada",
+        description: "Registra tus entradas y salidas de forma rápida y sencilla. Una interfaz intuitiva que garantiza fichajes precisos y un control horario fiable para todo el equipo.",
+        imageSrc: `${baseUrl}registro-jornada.webp`,
+        imageAlt: "Herramienta de Fichajes",
+        theme: "purple",
+        badge: "Cumplimiento legislativo",
+        icon: "M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+    },
+    {
+        id: 8,
+        title: "Revision de fichajes",
+        description: "Revisa y gestiona todos los fichajes de tus empleados desde un solo lugar. Visualiza entradas y salidas, corrige errores y asegura la precisión de los registros diarios.",
+        imageSrc: `${baseUrl}revision-fichajes.webp`,
+        imageAlt: "Revision de fichajes",
+        theme: "purple",
+        badge: "Cumplimiento legislativo",
+        icon: "M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
     }
 ];
 
